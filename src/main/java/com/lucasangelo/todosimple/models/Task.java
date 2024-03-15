15f67a95b016066d458cdb1b94411d0d2ba1b8da
ricardovinicius <108153768+ricardovinicius@users.noreply.github.com>
2024-03-15 15:08:02 -0300

    aula 16 adding lombok

--- src/main/java/com/lucasangelo/todosimple/models/Task.java
+++ src/main/java/com/lucasangelo/todosimple/models/Task.java
@@ -10,10 +10,13 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.Objects;
+
+import lombok.Data;
+
 
 @Entity
 @Table(name = Task.TABLE_NAME)
+@Data
 public class Task {
     public static final String TABLE_NAME = "task";
 
@@ -31,54 +34,4 @@ public class Task {
     @Size(min = 1, max = 255)
     private String description;
 
-
-    public Task() {
-    }
-
-    public Task(Long id, User user, String description) {
-        this.id = id;
-        this.user = user;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return this.user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof Task)) {
-            return false;
-        }
-        Task task = (Task) o;
-        return Objects.equals(id, task.id) && Objects.equals(user, task.user) && Objects.equals(description, task.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, user, description);
-    }
-    
 }
--- src/main/java/com/lucasangelo/todosimple/models/User.java
+++ src/main/java/com/lucasangelo/todosimple/models/User.java
@@ -11,18 +11,19 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-
 
 
 @Entity
 @Table(name = User.TABLE_NAME)
+@Data
 public class User {
     public interface CreateUser {}
     public interface UpdateUser {}
@@ -46,73 +47,9 @@ public class User {
     private String password;
 
     @OneToMany(mappedBy = "user")
+    @JsonProperty(access = Access.WRITE_ONLY)
     private List<Task> tasks = new ArrayList<Task>();
 
-
-    // BOILERPLATE:
-
-    public User() {
-    }
-
-    public User(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
     
-    @JsonIgnore
-    public List<Task> getTasks() {
-        return this.tasks;
-    }
 
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof User)) {
-            return false;
-        }
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password);
-    }
-
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-
-        return result;
-    }
-    
 }
