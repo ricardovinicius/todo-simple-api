6f30e4783b2f16e7b4045c4353563c15b6ae39bf
ricardovinicius <108153768+ricardovinicius@users.noreply.github.com>
2024-04-09 11:33:55 -0300

    create UserSpringSecurity

---
+++ src/main/java/com/lucasangelo/security/UserSpringSecurity.java
@@ -0,0 +1,61 @@
+package com.lucasangelo.security;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.lucasangelo.todosimple.models.enums.ProfileEnum;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class UserSpringSecurity implements UserDetails {
+    
+    private Long id;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserSpringSecurity(Long id, String username, String password, Set<ProfileEnum> profileEnums) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = profileEnums.stream().map(x -> new SimpleGrantedAuthority(x.getDescription())).collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public boolean hasRole(ProfileEnum profileEnum) {
+        return getAuthorities().contains(new SimpleGrantedAuthority(profileEnum.getDescription()));
+    }
+    
+}
