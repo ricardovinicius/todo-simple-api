9a7ed8517d9c2054e3e93b10eca858f1a8c1d686
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2024-04-14 22:13:56 -0300

    create jwtauthorizationfilter

--- src/main/java/com/lucasangelo/todosimple/configs/SecurityConfig.java
+++ src/main/java/com/lucasangelo/todosimple/configs/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.lucasangelo.todosimple.security.JWTAuthenticationFilter;
+import com.lucasangelo.todosimple.security.JWTAuthorizationFilter;
 import com.lucasangelo.todosimple.security.JWTUtil;
 
 @Configuration
@@ -62,6 +63,7 @@ public class SecurityConfig {
             .authenticationManager(authenticationManager);
 
         http.addFilter(new JWTAuthenticationFilter(this.authenticationManager, this.jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(this.authenticationManager, this.jwtUtil, this.userDetailsService));
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
